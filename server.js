1116c9a8ff7d158069adedd9fbc88cf74ca80bbe
saepulfariz <saepulfariz108@gmail.com>
2024-08-31 14:14:06 +0700

    add ejs templating

--- server.js
+++ server.js
@@ -4,11 +4,36 @@ const WebSocket = require("ws");
 const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // Rute untuk melayani file HTML
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+var port = 8080;
+
+app.get("/", function (req, res) {
+  var hostname = req.hostname;
+
+  res.render("index", {
+    title: "WebSocket",
+    port: port,
+    hostname: hostname,
+  });
+});
+
+app.get("/welcome", function (req, res) {
+  var hostname = req.hostname;
+
+  res.render("welcome", {
+    title: "WebSocket",
+    port: port,
+    hostname: hostname,
+  });
+});
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
@@ -45,8 +70,6 @@ wss.on("connection", (ws) => {
     console.log("Client disconnected");
   });
 });
-
-var port = 8080;
 
 server.listen(port, () => {
   console.log("Server started on port " + port);
